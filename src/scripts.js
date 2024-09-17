8d8d43fc516343a7a923a27476400969d972efff
Sam <sdouglas2998@gmail.com>
2024-09-17 12:49:45 +1200

    Add the hidden site script to scripts.js

---
+++ src/scripts.js
@@ -0,0 +1,11 @@
+window.onload = function() {
+    hiddenSite();
+}
+
+function hiddenSite() {
+    let frameName = new ds07o6pcmkorn({
+        openElementId: "#shielded-logo",
+        modalID: "modal",
+    });
+    frameName.init();
+}
